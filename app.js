d565e680e4c5602a3cb49dab9f33fbd3e5a6f483
chat-siddharth-com <121688939+chat-siddharth-com@users.noreply.github.com>
2022-12-30 23:40:26 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const messageForm = document.querySelector('#message-form');
+const messageInput = document.querySelector('#message-input');
+const messagesContainer = document.querySelector('#messages');
+
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get the message from the input
+  const message = messageInput.value;
+
+  // Create a new message element
+  const messageElement = document.createElement('div');
+  messageElement.innerHTML = message;
+
+  // Add the message to the messages container
+  messagesContainer.appendChild(messageElement);
+
+  // Clear the message input
+  messageInput.value = '';
+});
